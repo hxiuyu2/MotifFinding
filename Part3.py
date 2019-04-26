e4e5cfb50d2826e835d3d35e958cb36bd3f4e037
YananShen <46902985+YananShen@users.noreply.github.com>
2019-04-26 01:24:05 -0500

    Add files via upload

--- Part3.py
+++ Part3.py
@@ -2,13 +2,13 @@ from scipy import *
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-# import Part2 as p2
+#import Part2 as p2
 
 
 def compute_dkl():
-    res = [[]]*7
+    res = []
     for i in range(70):
-        with open("benchmark/dataset{}/motif.txt".format(str(i)),"r") as file1, open ("prediction/dataset{}/predictedmotif.txt".format(str(i)),"r") as file2:
+        with open("benchmark/dataset{}/motif.txt".format(str(i)),"r") as file1, open ("MEME/dataset{}/predictedmotif.txt".format(str(i)),"r") as file2:
             next(file1)
             next(file2)
             line1 = file1.readline()
@@ -27,66 +27,92 @@ def compute_dkl():
 
                 line1 = file1.readline()
                 line2 = file2.readline()
-            res[i%7].append(sum)
+            res.append(sum)
     return res
 
 
-def get_position():
-    result = [[]]*7
+def compute_position():
+    result = []
     for i in range(70):
-        # read motif length from 'motiflength.txt'
-        motif_len_txt = open('benchmark/dataset{}/motiflength.txt'.format(str(i)))
-        motif_len = int(motif_len_txt.readline().strip())
-        motif_len_txt.close()
+        with open("benchmark/dataset{}/motif.txt".format(str(i)), "r") as file1, open(
+                "MEME/dataset{}/predictedsites.txt".format(str(i)), "r") as file3, open(
+            "benchmark/dataset{}/sites.txt".format(str(i)), "r") as file2:
+            num = int(file1.readline().split()[1])
 
-        # get number of overlap position
-        predicted = open('prediction/dataset{}/predictedsites.txt'.format(str(i)))
-        true_site = open('benchmark/dataset{}/sites.txt'.format(str(i)))
-        for k in range(motif_len):
-            pred_val = int(predicted.readline().strip())
-            true_val = int(true_site.readline().strip())
-            positions = motif_len - abs(pred_val - true_val)
-            result[i%7].append(positions)
-        predicted.close()
-        true_site.close()
+            line1 = file2.readline()
+            line2 = file3.readline()
+
+            while line1 and line2:
+                P = line1.split()
+                Q = line2.split()
+
+                if int(P[0]) < int(Q[0]):
+                    temp = int(P[0]) + num - int(Q[0])
+                else:
+                    temp = int(Q[0]) + num - int(P[0])
+                if temp <= 0:
+                    temp = 0
+
+                temp += temp
+                line1 = file2.readline()
+                line2 = file3.readline()
+            result.append(temp)
     return result
 
 
-def get_overlap():
-    result = [[]] * 7
+
+def comput_overlap():
+    result = []
     for i in range(70):
-        # get number of overlap position
-        predicted = open('prediction/dataset{}/predictedmotif.txt.txt'.format(str(i)))
-        true_motif = open('benchmark/dataset{}/motif.txt'.format(str(i)))
-
-        # get motif length from the first line of motif.txt
-        motif_len = int(true_motif.readline().strip().split()[1])
-        match_cnt = 0
-        for k in range(motif_len):
-            pred_val = predicted.readline().strip()
-            true_val = true_motif.readline().strip()
-
-            # convert to float list
-            pred_val = np.array([float(i) for i in pred_val])
-            true_val = np.array([float(i) for i in true_val])
-
-            # get site[k]
-            pred_alpha = np.argmax(pred_val)
-            true_alpha = np.argmax(true_val)
-            if pred_alpha == true_alpha:
-                match_cnt += 1
-        result[i%7].append(match_cnt)
-        predicted.close()
-        true_motif.close()
+        with open("benchmark/dataset{}/sequences.fa".format(str(i)), "r") as file1, open(
+                "MEME/dataset{}/predictedsites.txt".format(str(i)), "r") as file3, open(
+            "benchmark/dataset{}/sites.txt".format(str(i)), "r") as file2, open(
+            "benchmark/dataset{}/motiflength.txt".format(str(i)),"r") as file4:
+            length = int(file4.readline().split()[0])
+            Str = file1.read()
+            line1 = file2.readline()
+            line2 = file3.readline()
+            res_count=0
+            while line1 and line2:
+                P = line1.split()
+                Q = line2.split()
+                original = Str[int(P[0]):int(P[0])+length-1]
+                predict = Str[int(Q[0]):int(Q[0])+length-1]
+                index1=0
+                index2=0
+                count=0
+                temp_str=""
+                #print(original,predict)
+                while index1 < len(original) and index2 < len(predict):
+                    if original[index1] == predict[index2]:
+                        count+=1
+                    index1+=1
+                    index2+=1
+                if count >= length / 2:
+                    res_count+=1
+                line1 = file2.readline()
+                line2 = file3.readline()
+            result.append(res_count)
     return result
-#
-#
-# def get_runtime():
-#     start = time.time()
-#     # TODO:
-#     # code for part 2
-#     end = time.time()
-#     return end - start
+
+
+def seven_list(List):
+    res = []
+    for i in range(0,7):
+        temp = []
+        for j in range(i,len(List),7):
+            temp.append(List[j])
+        res.append(temp)
+
+    return res
+
+
+def get_runtime():
+    start = time.time()
+    # TODO:
+    # code for part 2
+    end = time.time()
+    return end - start
 
 
 def avg_std(metrics):
@@ -95,7 +121,38 @@ def avg_std(metrics):
     return means, stds
 
 
+
 def draw(y, filename):
     plt.clf()
     plt.plot(range(len(y)), y)
     plt.savefig(filename)
+
+
+
+if __name__ == '__main__':
+
+    dkl = compute_dkl()
+    print(len(dkl))
+    print(dkl)
+    dkl_final = seven_list(dkl)
+
+    position = compute_position()
+    print(len(position))
+    print(position)
+    position_final = seven_list(position)
+
+    overlap = comput_overlap()
+    print(len(overlap))
+    print(overlap)
+    overlap_final = seven_list(overlap)
+
+
+    print(dkl_final)
+    print(position_final)
+    print(overlap_final)
+
+    print('average + std for dkl: ', avg_std(dkl_final))
+    print('average + std for position: ', avg_std(position_final))
+    print('average + std for overlap: ', avg_std(overlap_final))
+
+
