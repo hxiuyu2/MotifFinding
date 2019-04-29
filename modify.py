9c6f395512adca15aae12cfc0fa749dabef6733d
hxiuyu2 <beauniverse19@gmail.com>
2019-04-29 11:22:31 -0500

    change sequences file

--- modify.py
+++ modify.py
@@ -1,4 +1,24 @@
+# for i in range(70):
+#     file = open('benchmark/dataset{}/motif.txt'.format(str(i)), 'wr+')
+#     ind = 0
+#     contents = []
+#     for line in file:
+#         line = '>sequence'+str(ind)+'\n'+line
+#         contents.append(line)
+#         ind += 1
+#     contents = ''.join(contents)
+#     file.write(contents)
+#     file.close()
 for i in range(70):
-    file = open('benchmark/dataset{}/motif.txt'.format(str(i)), 'r+')
-    content = file.read()
+    file = open('benchmark/dataset{}/sequences.fa'.format(str(i)), 'r')
+    ind = 0
+    contents = []
+    for line in file:
+        line = '>sequence'+str(ind)+'\n'+line
+        contents.append(line)
+        ind += 1
+    file.close()
+    file = open('benchmark/dataset{}/sequences.fa'.format(str(i)), 'w')
+    contents = ''.join(contents)
+    file.write(contents)
     file.close()
